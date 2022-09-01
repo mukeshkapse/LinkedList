60e4c8028721bbe5ce76ff7bbc1f40cbf753ad04
mukeshkapse <mukeshkapse1832@gmail.com>
2022-09-02 01:21:42 +0530

    Added: list by Appending

--- src/com/bridgelabz/linklist/LinlkList.java
+++ src/com/bridgelabz/linklist/LinlkList.java
@@ -13,10 +13,10 @@ class Node{
 
 class operations{
 
-
-     //Taking variables to store the head
+    // Taking variables to store the head
 
     Node head;
+    Node tail;
 
     public void pushData(int data) {
 
@@ -26,7 +26,6 @@ class operations{
         if(head == null) {
 
             head=newnode;
-
         }
         else {
 
@@ -55,7 +54,20 @@ class operations{
 
         }
 
+    }
 
+    public void append(int data) {
+
+        Node newnode=new Node(data);
+        if (head == null) {
+            head=newnode;
+            tail=newnode;
+        }
+        else {
+            tail.next=	newnode;
+            tail=newnode;
+
+        }
     }
 
 }
@@ -63,13 +75,54 @@ class operations{
 
 public class LinlkList {
 
-    public static void main(String[] args) {
-
-        System.out.println("*** Welcome To LinkList Program ***");
-
+    public static void addDataStart() {
         operations link=new operations ();
         link.pushData(70);
         link.pushData(30);
         link.pushData(56);
         link.print();
-    }}
+    }
+
+    public static void addDataEnd() {
+        operations link=new operations ();
+        link.append(56);
+        link.append(30);
+        link.append(70);
+        link.print();
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("*** Welcome To LinkList Program ***");
+        Scanner sc= new Scanner(System.in);
+        System.out.println();
+        System.out.println("Press 1 to add data at start");
+        System.out.println("Press 2 to add data at end");
+        int input=sc.nextInt();
+
+        switch (input) {
+
+             //if user press 1 then addDataStart method will execute data will be store at starting
+
+            case 1:
+                addDataStart() ;
+                break;
+
+
+             //if user press 2 then addDataEnd method will execute data will be store
+
+
+            case 2:
+
+                addDataEnd();
+                break;
+
+            default:
+                System.out.println("Invalid Choice");
+
+        }
+
+    }
+
+}
