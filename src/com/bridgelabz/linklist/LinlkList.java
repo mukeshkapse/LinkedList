bc93827423a653dfc56c7dfea49e1b6c1be09a26
mukeshkapse <mukeshkapse1832@gmail.com>
2022-09-02 01:01:02 +0530

    Added: Created Simple link list And Added 56 30 70

--- src/com/bridgelabz/linklist/LinlkList.java
+++ src/com/bridgelabz/linklist/LinlkList.java
@@ -1,7 +1,36 @@
 package com.bridgelabz.linklist;
 
+class Node{
+    int data;
+    Node next;
+    public Node(int data) {
+        this.data=data;
+        this.next=null;
+    }
+}
+
 public class LinlkList {
+
     public static void main(String[] args) {
 
-        System.out.println("  Welcome To LinkList Program  ");
-}}
+        System.out.println("*** Welcome To LinkList Program ***");
+
+        Node firstnode = new Node(56);
+        Node secondnode = new Node(30);
+        Node thirdnode = new Node(70);
+
+        // created a head variable to store the first node address
+        //then we are storing the address of next nodes in variable next
+
+        Node head = firstnode;
+        firstnode.next = secondnode;
+        secondnode.next = thirdnode;
+
+        Node temp = head;// storing the head address into temp variable for print the values
+        while (temp != null) {
+            System.out.print(" -> " + temp.data);
+            temp = temp.next;
+        }
+
+    }
+}
